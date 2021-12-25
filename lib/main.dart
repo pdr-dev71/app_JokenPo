aeeb4fc211fe24071711333ef3eacca82813b6a5
pdr-dev71 <joaopedrosr2699@gmail.com>
2021-12-25 13:19:04 -0300

    added logic and fixed sintax erros and minors bugs solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:jokenpo/game.dart';
+import 'package:jokenpo/jogo.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Game(),
+    home: Jogo(),
     debugShowCheckedModeBanner: false,
   ));
 }
